8d9fda5761ef688eae49e4c85dd0a2007ed15d1e
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-11-25 12:26:36 +0100

    GitIgnore Refresh

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/clare/Documents/GitHub/Computer-Science-Project
-# Build directory: C:/Users/clare/Documents/GitHub/Computer-Science-Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/Codility")
--- build/src/Codility/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/clare/Documents/GitHub/Computer-Science-Project/src/Codility
-# Build directory: C:/Users/clare/Documents/GitHub/Computer-Science-Project/build/src/Codility
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
