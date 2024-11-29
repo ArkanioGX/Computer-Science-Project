d94865dff05e9972d8e43e8afd8167c180ffef4a
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-11-29 18:27:20 +0100

    Pathfinding Finished

--- src/CityTravel/City Travel.cpp
+++ src/CityTravel/City Travel.cpp
@@ -4,6 +4,9 @@
 
 #define inf std::numeric_limits<int>::max();
 
+std::vector<City*> cityExplored; 
+std::vector<pathFindingData*> pathAvailable; 
+
 
 
 Path* getMostValuablePath(City* cityStart, std::vector<City*> unvisitedCity);
@@ -12,13 +15,16 @@ std::vector<City*> City::list = {};
 
 void findPath()
 {
-
+    std::cout << "=====- Travelling Merchant -=====" << std::endl;
+    std::cout << "===- City Creation :" << std::endl << std::endl;
     //City Creations
     City* cityA = new City("A");
     City* cityB = new City("B");
     City* cityC = new City("C");
     City* cityD = new City("D");
     City* cityE = new City("E");
+
+    std::cout << std::endl << "===- Paths Creation :" << std::endl << std::endl;
 
     //A Paths
     Path* path = new Path(cityA, cityB, 2);
@@ -29,39 +35,117 @@ void findPath()
     path = new Path(cityB, cityD, 3);
     path = new Path(cityB, cityE, 1);
 
+    //D Paths
+    path = new Path(cityD, cityE, 3);
+
     //C Paths
     path = new Path(cityC, cityD, 1);
     path = new Path(cityC, cityE, 3);
 
-    //D Paths
-    path = new Path(cityD, cityE, 3);
+    
 
 
 
-    std::vector<pathFindingData*> cityRemaining;
     std::vector<Path*> pathFound;
     City* start = cityA;
-    City* check = start;
+    City* currentCity = start;
 
-{
     std::vector<City*> cityList = City::list; 
-    for (int i = 0; i < cityList.size(); i++){
-        if (cityList[i] != start){
-            pathFindingData* pathData = new pathFindingData();
-            pathData->dest = cityList[i];
-            pathData->currDist = inf;
-            cityRemaining.push_back(pathData);
-        }
-    }
 
-    std::cout << "test";
-}
     
+    cityExplored.push_back(currentCity);
 
     //Pathfinding Begin
 
-    while (cityRemaining.size() != 0) {
+    while (cityExplored.size() != cityList.size()) {
+        pathAvailable.clear();
+        findSubPath(currentCity);
+        pathFindingData* shortestPath = findLowestDistancePath(pathAvailable);
+
+        cityExplored.push_back(shortestPath->dest);
         
+
+        for (int i = 0; i < shortestPath->pathUsed.size(); i++){
+            pathFound.push_back(shortestPath->pathUsed[i]);
+        }
+
+        currentCity = shortestPath->dest;
+    }
+
+    //Remove the first so it can find his way back to start
+    cityExplored.erase(cityExplored.begin());
+    pathAvailable.clear();
+    findSubPath(currentCity);
+    pathFindingData* shortestPath = findLowestDistancePath(pathAvailable);
+
+    cityExplored.push_back(shortestPath->dest);
+
+    for (int i = 0; i < shortestPath->pathUsed.size(); i++){
+        pathFound.push_back(shortestPath->pathUsed[i]);
+    }
+    std::cout << std::endl << "===- Paths Found :" << std::endl << std::endl;
+    currentCity = start;
+    float pathDistance = 0;
+    for (int i = 0; i < pathFound.size(); i++){
+        std::cout << currentCity->getName() << " ---> " << pathFound[i]->getOtherCity(currentCity)->getName() << "( distance : " << pathDistance << " (+" << pathFound[i]->getDistance() << "))" << std::endl;
+        pathDistance += pathFound[i]->getDistance();
+        currentCity = pathFound[i]->getOtherCity(currentCity);
+    }
+    std::cout << std::endl << "===- Total Distance = " << pathDistance << std::endl;
+}
+
+void findSubPath(City* currentCity, pathFindingData lastPfd )
+{   
+    std::vector<City*>::iterator cityExploredIt = std::find(cityExplored.begin(),cityExplored.end(),currentCity);
+    if (cityExploredIt == cityExplored.end())
+    {
+        std::vector<pathFindingData*>::iterator pathFoundIt = otherPathLeadingToThisCity(lastPfd);
+        //Check if a path is already leading to this city
+        if (pathFoundIt != pathAvailable.end()){
+            //Compare the distance of paths to determine which one will be used
+            if (lastPfd.currDist > (*pathFoundIt)->currDist){
+                return;
+            }
+            pathAvailable.erase(pathFoundIt);
+        }
+        pathFindingData* newPath = new pathFindingData(lastPfd);
+        pathAvailable.push_back(newPath);
+        return;
+    }
+    else{
+        //Explored City Continue
+        std::vector<Path*> cityPaths = currentCity->getPaths();
+        for (int i = 0; i < cityPaths.size(); i++){
+            //Return if its the path ur coming from
+            if (cityPaths[i]->getOtherCity(currentCity) == lastPfd.getLastExploredCity()){
+                return;
+            }
+            //Return if this city is already explored in the current path
+            if (lastPfd.hasExploredThisCity(cityPaths[i]->getOtherCity(currentCity))){
+                return;
+            }
+            pathFindingData newPfd = lastPfd;
+            newPfd.addToData(currentCity,cityPaths[i]);
+            findSubPath(cityPaths[i]->getOtherCity(currentCity),newPfd);
+        }
     }
 }
 
+pathFindingData* findLowestDistancePath(std::vector<pathFindingData *> pathList)
+{
+    float minDist = inf;
+    pathFindingData* minDistPath = nullptr;
+    for (int i = 0; i < pathList.size(); i++){
+        if (pathList[i]->currDist < minDist){
+            minDist = pathList[i]->currDist;
+            minDistPath = pathList[i];
+        }
+    }
+    return minDistPath;
+}
+
+std::vector<pathFindingData *>::iterator otherPathLeadingToThisCity(pathFindingData pfd)
+{
+    std::vector<pathFindingData*>::iterator it = std::find(pathAvailable.begin(),pathAvailable.end(),&pfd) ;
+    return it;
+}
--- src/CityTravel/City Travel.h
+++ src/CityTravel/City Travel.h
@@ -13,6 +13,7 @@ public:
          };
 
     void addPath(class Path* p) { if (std::find(pathsList.begin(), pathsList.end(),p) == pathsList.end()) pathsList.push_back(p); }
+    std::vector<class Path*> getPaths() { return pathsList;}
 
     std::string getName() { return name; }
 
@@ -54,12 +55,44 @@ private:
 
 struct pathFindingData {
 
+    std::vector<Path*> pathUsed;
+    bool bIsUnexploredCity = false;
     City* dest;
-    float currDist;
+    float currDist = 0;
 
     bool operator==(const pathFindingData& pfd) {
+
         return pfd.dest == dest;
     }
+
+    void addToData(City* currentCity, Path* newPath){
+        dest = newPath->getOtherCity(currentCity);
+        pathUsed.push_back(newPath);
+        currDist += newPath->getDistance();
+    }
+
+    City* getLastExploredCity (){
+        if (pathUsed.size() == 0) {return nullptr;}
+        return pathUsed.back()->getOtherCity(dest);
+    }
+
+    bool hasExploredThisCity(City* cityToCheck){
+        City* c = cityToCheck;
+        for (int i = pathUsed.size()-1; i >= 0; i--){
+            c = pathUsed[i]->getOtherCity(c);
+            if (cityToCheck == c){
+                return true;
+            }
+        }
+        return false;
+    }
+
 };
 
 void findPath();
+
+void findSubPath(City* currentCity, pathFindingData lastPfd= pathFindingData());
+
+pathFindingData* findLowestDistancePath(std::vector<pathFindingData *> pathList);
+
+std::vector<pathFindingData*>::iterator otherPathLeadingToThisCity(pathFindingData pfd);
